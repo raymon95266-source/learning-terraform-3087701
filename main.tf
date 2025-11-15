395c251c86de96ff679445207ca1891eebb6b559
raymon95266-source <raymon95266@gmail.com>
2025-11-15 17:02:49 +0530

    security group

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "blog" {
     security_group_id = aws_security_group.blog.id
   }
 
-    resource = data.aws_security_group_rule" "blog_https_in" {
+    resource "aws_security_group_rule" "blog_https_in" {
     type   = "ingress"
     from_port = 443
     to_port = 443
@@ -57,7 +57,7 @@ resource "aws_security_group" "blog" {
   }
 
 
-  resource = data.aws_security_group_rule" "blog_everything_out" {
+  resource "aws_security_group_rule" "blog_everything_out" {
     type   = "egress"
     from_port = 0
     to_port = 80
